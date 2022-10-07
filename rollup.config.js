94eaefaa15e84d64e4b5062cc43e6f1d098ccfe7
tahar belghitri <tahartlm0@gmail.com>
2022-10-07 16:41:05 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import ts from "typescript";
+import sucrase from "@rollup/plugin-sucrase";
+
+// this override is needed because Module format cjs does not support top-level await
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require("./package.json");
+
+const globals = {
+  ...packageJson.devDependencies,
+};
+
+export default {
+  input: "index.ts",
+  output: [
+    {
+      file: "build/index.js",
+      format: "cjs", // commonJS
+      sourcemap: true,
+    },
+    {
+      file: "build/index.es.js",
+      format: "esm", // ES Modules
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      typescript: ts,
+    }),
+    // sucrase({
+    //   exclude: ["node_modules/**"],
+    //   transforms: ["typescript", "js"],
+    // }),
+    commonjs({
+      exclude: "node_modules",
+      ignoreGlobal: true,
+    }),
+  ],
+  external: Object.keys(globals),
+};
